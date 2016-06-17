774f8730ff33f810cd7d97955475e86f1ac7c17e
Johannes Oos <j.oos@hotmail.de>
2016-06-17 17:14:52 +0300

    adjust timers

--- index_timing.py
+++ index_timing.py
@@ -3,19 +3,33 @@ import pymongo
 import sys
 import time 
 
+#start timer
+t0 = time.time()
+
 # establish a connection to the database
 connection = pymongo.MongoClient("mongodb://localhost")
 
-#create new databae
-connection.create
+#time creation of conention
+t1 = time.time()
 
-# get a handle to the teh new sample database and coleltion
+
+# get a handle to the th new sample database and coleltion
+connection.drop_database("new_test")
+
+t2 = time.time()
+
 db=connection.new_test
 co = db.index_text
+# get time for creaetion (is lazy so nothing shoudl have happend)
+t3 = time.time()
 
-co.insert_one({"test":2})
+for a in range(100):
+    for b in range(10):
+        for c in range(10):
+            co.insert_one({"a":a, "b": b, "c":c})
 
-t0 = time.time()
+#inserted the 10,000 documents
+t4 = time.time()
 
 #def find():
 #
