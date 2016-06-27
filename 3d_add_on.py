aad5ac271c94c336a8c2db65d842f6b880d139f5
Johannes Oos <j.oos@hotmail.de>
2016-06-27 22:04:51 +0300

    first commit

---
+++ 3d_add_on.py
@@ -0,0 +1,18 @@
+
+import pymongo
+
+# establish a connection to the database
+connection = pymongo.MongoClient("mongodb://localhost")
+
+
+
+# use the correct database
+db=connection.geo_trial
+
+#use the set up trial database
+geo = db.trial_one
+
+#insert first coordinate
+geo.insert_one({"loc" : { "lng" : 0.1 , "lat" : 0.1 }})
+
+
