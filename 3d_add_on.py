c07efd25e47c910a975c465ca1f3e5fd0d7d249c
Johannes Oos <j.oos@hotmail.de>
2016-06-28 16:54:55 +0300

    adjust query more flexible

--- 3d_add_on.py
+++ 3d_add_on.py
@@ -21,10 +21,17 @@ for a in range(10):
         for c in range(10):
 #            geo.insert_one({"loc" : { "lng" : a , "lat" : b }, "height": c })
         # better to be don ewith list
-            geo.insert_one({"loc" : [a,b]})
-            
+            geo.insert_one({"loc" : [a,b], "height": c})
+
+#create index       
 geo.create_index( [( "loc", "2d" ) ])
 
+# create query for in rectangle
+query = {"loc": {"$within": {"$box": [[3, 3], [5, 5]]}}}
+
+#query for 2d results
+for place in geo.find(query):
+    print place
 
         
 
